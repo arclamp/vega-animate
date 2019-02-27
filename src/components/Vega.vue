dca962ed749e9beffa06bf351d86e5d92fccd0fd
Roni Choudhury <roni.choudhury@kitware.com>
2019-02-27 11:17:37 -0500

    Add a working vega component

---
+++ src/components/Vega.vue
@@ -0,0 +1,17 @@
+<template>
+  <div ref="vega"></div>
+</template>
+
+<script>
+  import { parse, View } from 'vega';
+  import spec from '@/assets/spec.vg.json';
+
+  export default {
+    mounted () {
+      this.vega = new View(parse(spec))
+        .initialize(this.$refs.vega)
+        .renderer('svg')
+        .run();
+    }
+  };
+</script>
